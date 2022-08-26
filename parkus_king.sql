b5d2f8ef4923cff934a074fca38431e5df43b637
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-08-25 21:31:59 -0300

    descontinuação do projeto

--- parkus_king.sql
+++
@@ -1,8 +0,0 @@
-USE `parkus_king`;
-CREATE DATABASE	`parkus_king`;
-
-CREATE TABLE `usuario` (
-	`id` INTEGER NOT NULL PRIMARY KEY,
-	`nome` VARCHAR(100) NOT NULL,
-	`email` VARCHAR(80) NULL UNIQUE
-);
